c92f95b7ecf4e07138563a747dec27b5cb12576d
Chris Silich <chris.silich@gmail.com>
2020-01-20 13:23:17 -0500

    ready for students

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,38 +1,4 @@
-'use strict';
+"use strict";
 
-var $lightboxLinks = document.querySelectorAll('.lightboxify');
-
-$lightboxLinks.forEach(function ($lightboxLink) {
-	$lightboxLink.addEventListener('click', function (event) {
-		// prevent the link click from taking us away to another page
-		event.preventDefault();
-
-		// create the overlay
-		var $overlay = document.createElement('div');
-		$overlay.classList.add('lightbox-overlay');
-		document.body.appendChild($overlay);
-
-		// create the lightbox
-		var $lightbox = document.createElement('div');
-		$lightbox.classList.add('lightbox');
-		document.body.appendChild($lightbox);
-
-		// create the close button and put it in the lightbox
-		var $close = document.createElement('button');
-		$close.classList.add('lightbox-close');
-		$lightbox.appendChild($close);
-
-		// create the image and put it in the lightbox
-		var $image = document.createElement('img');
-		$image.classList.add('lightbox-image');
-		$image.setAttribute('src', this.getAttribute('href'));
-		$lightbox.appendChild($image);
-
-		// make the close button work
-		$close.addEventListener('click', function () {
-			document.body.removeChild($overlay);
-			document.body.removeChild($lightbox);
-		});
-	});
-});
+var lightboxLinks = document.querySelectorAll('.lightboxify');
 //# sourceMappingURL=main.js.map
