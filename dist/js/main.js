2bc4b36a1c370233f5b4b4c99b72f7e5bfa9e1b5
Chris Silich <chris.silich@gmail.com>
2020-01-20 13:22:44 -0500

    done for teacher demo

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,38 +1,32 @@
-'use strict';
+"use strict";
 
 var $lightboxLinks = document.querySelectorAll('.lightboxify');
-
 $lightboxLinks.forEach(function ($lightboxLink) {
-	$lightboxLink.addEventListener('click', function (event) {
-		// prevent the link click from taking us away to another page
-		event.preventDefault();
+  $lightboxLink.addEventListener('click', function (event) {
+    // prevent the link click from taking us away to another page
+    event.preventDefault(); // create the overlay
 
-		// create the overlay
-		var $overlay = document.createElement('div');
-		$overlay.classList.add('lightbox-overlay');
-		document.body.appendChild($overlay);
+    var $overlay = document.createElement('div');
+    $overlay.classList.add('lightbox-overlay');
+    document.body.appendChild($overlay); // create the lightbox
 
-		// create the lightbox
-		var $lightbox = document.createElement('div');
-		$lightbox.classList.add('lightbox');
-		document.body.appendChild($lightbox);
+    var $lightbox = document.createElement('div');
+    $lightbox.classList.add('lightbox');
+    document.body.appendChild($lightbox); // create the close button and put it in the lightbox
 
-		// create the close button and put it in the lightbox
-		var $close = document.createElement('button');
-		$close.classList.add('lightbox-close');
-		$lightbox.appendChild($close);
+    var $close = document.createElement('button');
+    $close.classList.add('lightbox-close');
+    $lightbox.appendChild($close); // create the image and put it in the lightbox
 
-		// create the image and put it in the lightbox
-		var $image = document.createElement('img');
-		$image.classList.add('lightbox-image');
-		$image.setAttribute('src', this.getAttribute('href'));
-		$lightbox.appendChild($image);
+    var $image = document.createElement('img');
+    $image.classList.add('lightbox-image');
+    $image.setAttribute('src', this.getAttribute('href'));
+    $lightbox.appendChild($image); // make the close button work
 
-		// make the close button work
-		$close.addEventListener('click', function () {
-			document.body.removeChild($overlay);
-			document.body.removeChild($lightbox);
-		});
-	});
+    $close.addEventListener('click', function () {
+      document.body.removeChild($overlay);
+      document.body.removeChild($lightbox);
+    });
+  });
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
-let lightboxLinks = document.querySelectorAll('.lightboxify');
+let $lightboxLinks = document.querySelectorAll('.lightboxify');
+
+$lightboxLinks.forEach(function ($lightboxLink) {
+	$lightboxLink.addEventListener('click', function (event) {
+		// prevent the link click from taking us away to another page
+		event.preventDefault();
+
+		// create the overlay
+		let $overlay = document.createElement('div');
+		$overlay.classList.add('lightbox-overlay');
+		document.body.appendChild($overlay);
+
+		// create the lightbox
+		let $lightbox = document.createElement('div');
+		$lightbox.classList.add('lightbox');
+		document.body.appendChild($lightbox);
+
+		// create the close button and put it in the lightbox
+		let $close = document.createElement('button');
+		$close.classList.add('lightbox-close');
+		$lightbox.appendChild($close);
+
+		// create the image and put it in the lightbox
+		let $image = document.createElement('img');
+		$image.classList.add('lightbox-image');
+		$image.setAttribute('src', this.getAttribute('href'));
+		$lightbox.appendChild($image);
+
+		// make the close button work
+		$close.addEventListener('click', function () {
+			document.body.removeChild($overlay)
+			document.body.removeChild($lightbox)
+		})
+	})
+})
